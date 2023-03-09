3ee9d434e1d43c1d5a25bd27d7826c8522d92c13
Dev Kumar <devkumar@Devs-MacBook-Pro.local>
2023-03-09 10:54:56 +0530

    Adding first HTML Structure

---
+++ src/components/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <img src='./../images/Logo.svg' alt='Little Lemons Logo'/>
+        <ul>
+            <li>
+                <a href='/#Header'>Header</a>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <a href='/#Main'>Main</a>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <a href='/#Footer'>Footer</a>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Nav
