da4568d3501bc819e8970f80cbc6453c1b35c488
Dev Kumar <devkumar@Devs-MacBook-Pro.local>
2023-03-12 15:48:30 +0530

    Adding imgaes and more components

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <>
       <Nav />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route index element={<Main />} />
         <Route path="/about" element={<About />} />
       </Routes>
       <Main />
